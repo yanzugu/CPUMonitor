6bb17d64adf446c2ace1c26bc25f7087b9d56cc0
yanzu <yanzugu@gmail.com>
2022-05-15 14:19:40 +0800

    update
    -hide form from Alt + Tab
    -make window not minimizable

--- Pages/ShellView.xaml.cs
+++ Pages/ShellView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace CPUMonitor.Pages
 {
@@ -20,8 +23,30 @@ namespace CPUMonitor.Pages
             }
         }
 
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        private const int GWL_EX_STYLE = -20;
+        private const int WS_EX_APPWINDOW = 0x00040000, WS_EX_TOOLWINDOW = 0x00000080;
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            Window window = sender as Window;
+
+            if (window.WindowState == WindowState.Minimized)
+            {
+                window.WindowState = WindowState.Normal;
+            }
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //Variable to hold the handle for the form
+            var helper = new WindowInteropHelper(this).Handle;
+            //Performing some magic to hide the form from Alt+Tab
+            SetWindowLong(helper, GWL_EX_STYLE, (GetWindowLong(helper, GWL_EX_STYLE) | WS_EX_TOOLWINDOW) & ~WS_EX_APPWINDOW);
+
             btnClose.PreviewMouseLeftButtonDown += (s, e) => { this.Close(); };
             btnPin.PreviewMouseLeftButtonDown += (s, e) => { this.Topmost = !this.Topmost; };
 
